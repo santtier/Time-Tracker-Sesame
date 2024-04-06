1bc04c4d7a1487ba7b63b07ffcfd03c9fe210a7f
santtier <santitodarello14@gmail.com>
2024-04-06 19:07:33 +0200

    do the logic when the user is online or not. Also create a helper function to parse the seconds to this format HH:mm:ss

--- src/components/TimeTracker.vue
+++ src/components/TimeTracker.vue
@@ -4,7 +4,7 @@
     <div class="flex justify-between">
       <div>
         <p class="text-gray-700 mb-2">Tiempo trabajado:</p>
-        <!-- <h3 class="text-2xl font-bold">{{ timeWorked }}</h3> -->
+        <h3 class="text-2xl font-bold">{{ formattedTimeWorked }}</h3>
       </div>
       <div>
         <Button :text="buttonText" @click="onButtonClick" />
@@ -15,8 +15,9 @@
 
 <script>
 import Button from "./Button.vue";
-import { mapActions } from "pinia";
+import { mapActions, mapState } from "pinia";
 import { useTimeTrackerStore } from '../store'
+import { formatTime } from "../helpers/helpers";
 
 export default {
   name: "TimeTracker",
@@ -24,14 +25,29 @@ export default {
     Button,
   },
   computed: {
+    ...mapState(useTimeTrackerStore, ['isClockedIn', 'timeWorked']),
+    formattedTimeWorked() {
+      return formatTime(this.timeWorked);
+    },
+    buttonText() {
+      return this.isClockedIn ? 'Salir' : 'Entrar'
+    }
   },
   data() {
     return {
-      buttonText: "Entrar",
     };
   },
   methods: {
-    ...mapActions(useTimeTrackerStore, ['getWorkEntries'])
+    ...mapActions(useTimeTrackerStore, ['getWorkEntries', 'clockIn', 'clockOut']),
+    onButtonClick() {
+      if (this.isClockedIn) {
+        this.clockOut()
+        this.buttonText = 'Entrar'
+      } else {
+        this.clockIn()
+        this.buttonText = 'Salir'
+      }
+    }
   },
   mounted() {
     this.getWorkEntries()
--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,66 @@ export const useTimeTrackerStore = defineStore('timeTracker', {
   state: () => ({
     isClockedIn: false,
     timeWorked: 0,
-    employeeId: "12345",
+    employeeId: "",
+    coordinates: {}
   }),
   actions: {
+    async clockIn() {
+      const workEntry = await api.post('/work-entries/clock-in', {
+        employeeId: this.employeeId,
+        workEntryIn: {
+          coordinates: this.coordinates
+        }
+      })
+
+      const workEntryData = await workEntry.data.data
+
+      this.timeWorked = workEntryData.workedSeconds
+      this.isClockedIn = true
+      clearInterval(this.interval)
+      this.interval = setInterval(() => {
+        this.timeWorked++
+      }, 1000);
+    },
+    async clockOut() {
+      const workEntry = await api.post("/work-entries/clock-out", {
+        employeeId: this.employeeId,
+        workEntryOut: {
+          coordinates: this.coordinates
+        },
+      });
+
+      const workEntryData = workEntry.data.data
+      clearInterval(this.interval)
+      this.isClockedIn = false
+      this.timeWorked = workEntryData.workedSeconds
+    },
     async getWorkEntries() {
       try {
         const response = await api.get('/work-entries');
 
-        console.log(response)
-      } catch {
+        const workEntriesData = response.data.data
+        const lastWorkEntry = workEntriesData[workEntriesData.length - 1]
+        const employeeData = lastWorkEntry.employee
         
+        this.employeeId = employeeData.id
+        this.coordinates = lastWorkEntry.workEntryIn.coordinates.latitude ? lastWorkEntry.workEntryIn.coordinates : lastWorkEntry.workEntryOut.coordinates
+        
+        if (lastWorkEntry) {
+          if (employeeData.workStatus === 'online') {
+            this.isClockedIn = true
+            this.timeWorked =  Math.floor((Date.now() - new Date(lastWorkEntry.workEntryIn.date).getTime()) / 1000);
+            clearInterval(this.interval)
+            this.interval = setInterval(() => {
+              this.timeWorked++
+            }, 1000);
+          } else {
+            this.isClockedIn = false
+            this.timeWorked = lastWorkEntry.workedSeconds
+          }
+        }
+      } catch {
+        console.error("Error getting work entries:");
       }
     }
   }
