288236b521dc7912fe1ba38940bac35df66670e0
santtier <santitodarello14@gmail.com>
2024-04-10 02:24:56 +0200

    pass styles in classes to tailwind

--- src/components/TimeTracker.vue
+++ src/components/TimeTracker.vue
@@ -14,18 +14,19 @@
         <div class="dropdown-container py-4 w-full h-full flex items-center">
           <div class="relative">
             <img class="h-10" src="@/assets/batman-avatar.webp" alt="avatar">
-            <div :class="{ 'bg-green-500': isClockedIn, 'bg-gray-400': !isClockedIn }" class="circle h-2 w-2"></div>
+            <div :class="{ 'bg-green-500': isClockedIn, 'bg-gray-400': !isClockedIn }" class="absolute -right-1 top-4
+ rounded-full h-2 w-2"></div>
           </div>
           <p class="truncate ml-2 font-semibold">{{ employee.name }}</p>
-          <span class="inline-block ml-3 svg-icon"><svg class="w-full h-full stroke-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z" fill="#000000"></path> </g></svg></span>
+          <span class="inline-block ml-3 w-4 h-4 min-w-4 min-h-4"><svg class="w-full h-full stroke-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z" fill="#000000"></path> </g></svg></span>
         </div>
-        <ul class="bg-white w-auto rounded-xl min-w-64 dropdown shadow rounded-t-none py-3">
+        <ul class="bg-white w-auto rounded-xl min-w-64 absolute right-2 dropdown shadow rounded-t-none py-3">
           <li class="flex cursor-pointer px-4 py-3 mb-2 text-sm items-center justify-between relative accounts hover:bg-gray-100">
-            <span class="flex-none svg-icon"><svg viewBox="0 0 1024 1024" class="icon stroke-current" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z" fill="#000000"></path></g></svg></span>
+            <span class="flex-none w-4 h-4 min-w-4 min-h-4"><svg viewBox="0 0 1024 1024" class="icon stroke-current" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z" fill="#000000"></path></g></svg></span>
             <p class="hover:text-blue-400 flex-1 text-center mr-2">
               <a href="#">Mis Cuentas</a>
             </p>
-            <ul class="absolute w-auto shadow min-w-64 bg-white py-3 rounded-xl rounded-r-none dropdown-second-level">
+            <ul class="absolute w-auto shadow min-w-64 bg-white py-3 rounded-xl rounded-r-none dropdown-second-level -top-3 right-64">
               <li class="px-4  hover:bg-gray-100" v-for="userAccount in userAccounts" :key="userAccount.account">
                 <div class="flex w-auto rounded-xl max-w-max items-center">
                   <div class="mr-2 h-12 w-12 flex items-center">
@@ -101,23 +102,11 @@ export default {
 
 <style scoped>
 .dropdown {
-  position: absolute;
-  right: 8px;
   display: none;
-  z-index: 50;
 }
 
 .dropdown-second-level {
-  top: -12px;
-  right: 256px;
   display: none;
-}
-
-.svg-icon {
-  height: 16px;
-  width: 16px;
-  min-height: 16px;
-  max-height: 16px;
 }
 
 .dropdown-container:hover + .dropdown {
@@ -130,13 +119,5 @@ export default {
 
 .accounts:hover .dropdown-second-level {
   display: block;
-}
-
-.circle {
-  position: absolute;
-  border-radius: 50%;
-  top: 50%;
-  left: 34px;
-  transform: translate(0, -50%);
 }
 </style>
